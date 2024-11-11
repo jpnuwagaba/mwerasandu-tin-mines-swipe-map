40d5dd75e67d0b22bfa727390d346df450c75bf6
jpnuwagaba <jpnuwagaba1@gmail.com>
2024-11-11 23:08:41 +0300

    fixed used declars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import MapCompare from "@/components/MapCompare";
-import SampleMap from "@/components/SampleMap";
-import Image from "next/image";
+// import SampleMap from "@/components/SampleMap";
+// import Image from "next/image";
 
 export default function Home() {
   return (
