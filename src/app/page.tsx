f017011ba6bc6d871b264a547fcdeadb1dde613e
John Peter Nuwagaba <95671518+jpnuwagaba@users.noreply.github.com>
2024-11-18 15:18:52 +0300

    Update page.tsx, next/dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import MapCompare from "@/components/MapCompare";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const MapCompare = dynamic(() => import("../components/MapCompare"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <>
-     <MapCompare />
+      <MapCompare />
     </>
   );
 }
