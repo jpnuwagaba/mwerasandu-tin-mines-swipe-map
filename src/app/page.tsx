4617d56a225e3cdeacffdccb0bc62eeca76b5e0c
jpnuwagaba <jpnuwagaba1@gmail.com>
2024-11-18 07:51:32 +0300

    fourth attempt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import MapCompare from "@/components/MapCompare";
-// import SampleMap from "@/components/SampleMap";
-// import Image from "next/image";
 
 export default function Home() {
   return (
     <>
      <MapCompare />
-     {/* <SampleMap /> */}
     </>
   );
 }
